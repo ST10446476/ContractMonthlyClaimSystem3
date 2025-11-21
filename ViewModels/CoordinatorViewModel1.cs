991d26cb7b154f2924ab4f819a1e0de2ca3f691a
orati <orati@LWETHUU>
2025-11-21 16:58:34 +0200

    Implement CoordinatorViewModel with approval workflow"

---
+++ ViewModels/CoordinatorViewModel1.cs
@@ -0,0 +1,7 @@
+﻿namespace ContractMonthlyClaimSystem.ViewModels
+{
+    internal class CoordinatorViewModel
+    {
+        public string? FilterStatus { get; internal set; }
+    }
+}
--- Views/CoordinatorView.xaml.cs
+++ Views/CoordinatorView.xaml.cs
@@ -1,18 +1,11 @@
-﻿using System;
+﻿using ContractMonthlyClaimSystem.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace ContractMonthlyClaimSystem.Views
+namespace ClaimSystem.Views
 {
     /// <summary>
     /// Interaction logic for CoordinatorView.xaml
@@ -22,6 +15,32 @@ namespace ContractMonthlyClaimSystem.Views
         public CoordinatorView()
         {
             InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            // Handle status filter radio button changes
+            if (DataContext is ContractMonthlyClaimSystem.ViewModels.CoordinatorViewModel viewModel)
+            {
+                var radioButton = sender as RadioButton;
+                if (radioButton != null && radioButton.Tag != null)
+                {
+                    viewModel.FilterStatus = radioButton.Tag.ToString();
+                }
+            }
+        }
+
+        protected override void OnClosed(System.EventArgs e)
+        {
+            base.OnClosed(e);
+            // Return to login
+            var login = new LoginWindow();
+            login.Show();
         }
     }
 }
